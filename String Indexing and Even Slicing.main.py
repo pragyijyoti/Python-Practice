2d7baea73a2851e72f03f8c91503ab305eb79b2a
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-11 13:51:47 -0500

    Add files via upload

---
+++ String Indexing and Even Slicing.main.py
@@ -0,0 +1,9 @@
+
+# Display only those characters which are present at an even index number in given string.
+
+string="Pynative"
+even=string[::2]
+
+print("The even index number in the given string is:")
+for e in even:
+    print(e)
