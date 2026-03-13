af953f38ae32316a862f56abcd6ec5e21bf3b23b
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-12 20:51:07 -0500

    Add files via upload

---
+++ String Reversal.main.py
@@ -0,0 +1,10 @@
+'''
+String Reversal
+
+
+Practice Problem: Write a program that takes a string and reverses it (e.g., “Python” becomes “nohtyP”).
+'''
+
+word="ilovegoogle"
+reversed=word[::-1]
+print(f"Reversed:{reversed}")
