7108e2c210598265ce82bad70ed654b448595181
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-12 20:45:20 -0500

    Add files via upload

---
+++ List Manipulation_ Add and Remove.main.py
@@ -0,0 +1,12 @@
+'''
+List Manipulation: Add and Remove
+
+Practice Problem: Create a list of 5 fruits. Add a new fruit to the end of the list, 
+then remove the second fruit (at index 1).
+'''
+
+fruits=["apple","banana","grapes","strawberry","watermelon"]
+fruits.append("blueberry")
+fruits.pop(1)
+
+print(fruits)
