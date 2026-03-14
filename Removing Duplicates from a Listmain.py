5670a54b33b0948294bbba94bb2c79111cc787db
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-14 00:23:22 -0500

    Add files via upload

---
+++ Removing Duplicates from a Listmain.py
@@ -0,0 +1,9 @@
+
+# Removing Duplicates from a List
+
+#Practice Problem: Write a script that takes a list containing duplicate items and 
+#returns a new list with only unique elements.
+
+my_list=[2,2,4,4,4,6,6,8,10,22,22]
+final=list(set(my_list))
+print(final)
