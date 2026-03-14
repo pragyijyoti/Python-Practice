d4183128bc13b366c236f9fcbf78f0e5c14ab28f
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-14 00:19:09 -0500

    Add files via upload

---
+++ Finding Extremes (Min_Max) in a Listmain.py
@@ -0,0 +1,8 @@
+
+# Finding Extremes (Min/Max) in a List
+#Practice Problem: Given a list of integers, find and print both the largest and the smallest numbers.
+
+my_list=[2,4,6,8,10,22]
+a=min(my_list)
+b=max(my_list)
+print(f"The minimum is {a} and the maximum is {b}")
