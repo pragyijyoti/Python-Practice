0f02f2b280c887a80f43c450aaddeebe4f0497ce
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-16 11:01:19 -0500

    Add files via upload

---
+++ Nested Loops for Pattern Generationmain.py
@@ -0,0 +1,17 @@
+
+'''Nested Loops for Pattern Generation
+
+Practice Problem: Print the following pattern where each row contains a number repeated a specific number
+of times based on its value.
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+'''
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+        
