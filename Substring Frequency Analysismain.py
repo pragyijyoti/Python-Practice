824fd020aed0b4d4508020d1bb2e27bc679ae0d3
Pragyi.ai <pragyijyoti@gmail.com>
2026-03-16 10:56:14 -0500

    Add files via upload

---
+++ Substring Frequency Analysismain.py
@@ -0,0 +1,10 @@
+
+# Substring Frequency Analysis
+
+#Practice Problem: Write a program to find how many times the substring “Emma” appears in a given string.
+
+paragraph=("Emma is strong and beautiful. The best female lead in AI depatment was awarded to Emma. Emma is very happy")
+
+n=paragraph.count("Emma")
+
+print("Number of times Emma appears=",n)
